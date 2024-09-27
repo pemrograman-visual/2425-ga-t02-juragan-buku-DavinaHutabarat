fd2373a15687a1810e72439bea62b2c81d0c9ead
DavinaHutabarat <dggxqh@gmail.com>
2024-09-27 11:08:32 +0700

    Sudah menyelesaikan tugas

--- T02.java
+++ T02.java
@@ -1,1 +1,46 @@
-// NIM - Your Name
+// 12S24047 - Davina Olivia Yosefanny Hutabarat
+// 12S24050 - Ester Hasianna Nainggolan
+
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judul, penulis;
+        int tahunterbit;
+        String penerbit;
+        String formatbukuelektronik, rate;
+        double rating;
+
+        isbn = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunterbit = input.nextInt();
+        penerbit = input.nextLine();
+        formatbukuelektronik = input.nextLine();
+        rating = input.nextDouble();
+        if (rating >= 4.7) {
+            rate = "Best Pick";
+        } else {
+            if (rating >= 4.5 && rating < 4.7) {
+                rate = "Must Read";
+            } else {
+                if (rating >= 4.0 && rating < 4.5) {
+                    rate = "Recommended";
+                } else {
+                    if (rating >= 3.0 && rating < 4.0) {
+                        rate = "Average";
+                    } else {
+                        if (rating < 3.0 && rating >= 0.0) {
+                            rate = "Low";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + rate);
+    }
+}
+
