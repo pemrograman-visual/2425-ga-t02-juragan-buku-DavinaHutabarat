62d518f6c0d41dbdc13042a62819d00a7b1839fa
DavinaHutabarat <dggxqh@gmail.com>
2024-09-27 14:10:05 +0700

    pair28

--- T02.java
+++ T02.java
@@ -1,20 +1,18 @@
 // 12S24047 - Davina Olivia Yosefanny Hutabarat
 // 12S24050 - Ester Hasianna Nainggolan
-
 import java.util.*;
 import java.lang.Math;
 
-public class T02 {
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String isbn, judul, penulis;
+        String isbn, judul, penulis, grade;
         int tahunterbit;
         String penerbit;
         String formatbukuelektronik;
         double hargapembelian, minimummargin;
         int stok;
-        String rate;
         double rating;
 
         isbn = input.nextLine();
@@ -28,25 +26,25 @@ public class T02 {
         stok = Integer.parseInt(input.nextLine());
         rating = Double.parseDouble(input.nextLine());
         if (rating >= 4.7) {
-            rate = "Best Pick";
+            grade = "Best Pick";
         } else {
             if (rating >= 4.5 && rating < 4.7) {
-                rate = "Must Read";
+                grade = "Must Read";
             } else {
                 if (rating >= 4.0 && rating < 4.5) {
-                    rate = "Recommended";
+                    grade = "Recommended";
                 } else {
-                    if (rating >= 3.0 && rating < 4.0) {
-                        rate = "Average";
+                    if (rating >= 3.0) {
+                        grade = "Average";
                     } else {
-                        if (rating < 3.0 && rating >= 0.0) {
-                            rate = "Low";
+                        if (rating < 3.0) {
+                            grade = "Low";
                         }
                     }
                 }
             }
         }
-        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + rate + "|" + toFixed(rating,1));
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + grade + "|" + toFixed(rating,1));
     }
     
     private static String toFixed(double value, int digits) {
